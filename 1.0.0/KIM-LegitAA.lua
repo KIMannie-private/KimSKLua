2ea28cc234b9f3eaaae252cac90034be5218c23d
KIMannie-private <67752144+KIMannie-private@users.noreply.github.com>
2020-07-03 01:20:22 +0800

    automatically upload on release

---
+++ 1.0.0/KIM-LegitAA.lua
@@ -0,0 +1,74 @@
+client.log("     Get good Get gamesense")
+client.color_log(0, 255, 216, "[gamesense] @author KIMAnnie")
+client.color_log(0, 255, 216, "[gamesense] ♛KIM-Beta.CFG♛")
+client.color_log(0, 255, 216, "[gamesense] AA lua V4: 2020/06/06")
+client.color_log(0, 255, 216, "[gamesense] CFG Update: 2020/06/06")
+client.color_log(0, 255, 0, "[gamesense] For premium&id=6259")
+
+local s = require "./♛KIM-Beta♛/KL- AimLog"
+local s = require "./♛KIM-Beta♛/helper"
+
+client.log("Wrapper Enabling...")
+local x = nil
+
+require("./♛KIM-Beta♛/KIM/KL- Fake Indicators")
+require("./♛KIM-Beta♛/KIM/KL- Legit AT")
+
+require("./♛KIM-Beta♛/KIM/KS- Best-LegalAA")
+require("./♛KIM-Beta♛/KIM/KS- skybox_changer")
+require("./♛KIM-Beta♛/KIM/KS- quick_peek")
+require("./♛KIM-Beta♛/KIM/KS- KimAnnieDT")
+
+require("./♛KIM-Beta♛/KIM/havoc_zeus_warning_config")
+require("./♛KIM-Beta♛/KIM/havoc_zeus_warning")
+
+require("./♛KIM-Beta♛/KIM/KL- KIM")
+require("./♛KIM-Beta♛/KIM/KL- hud print chat")
+require("./♛KIM-Beta♛/KIM/KL- grenade_esp")
+require("./♛KIM-Beta♛/KIM/KL- Indicators")
+require("./♛KIM-Beta♛/KIM/KL- havoc_announcer_1_4")
+require("./♛KIM-Beta♛/KIM/KL- ChenRainbow")
+require("./♛KIM-Beta♛/KIM/KL- watermark")
+
+
+
+
+x = require("./♛KIM-Beta♛/KIM/KS- adaptive")
+x.auth()
+require("./♛KIM-Beta♛/KIM/KS- alive3-Fix")
+
+require("./♛KIM-Beta♛/KIM/KK- Legit")
+require("./♛KIM-Beta♛/KIM/KK- Twin clantag")
+require("./♛KIM-Beta♛/KIM/KK- autobuy")
+require("./♛KIM-Beta♛/KIM/KK- KANTOUPIAO")
+
+
+
+--x = require("./♛KIM-Beta♛/KIM/KS- exploit")
+--x.auth()
+
+require("./♛KIM-Beta♛/KK- bloom")
+require("./♛KIM-Beta♛/KK- fudu")
+require("./♛KIM-Beta♛/KK- spam_Dabeizhou")
+require("./♛KIM-Beta♛/KL- AimLog")
+
+require("./♛KIM-Beta♛/neverlose")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.log("Wrapper Enabled")
+client.color_log(255, 0, 255, "All ♛KIM-MASTER.♛ luas r loaded")
